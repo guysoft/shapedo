64b1f2c49b5e633c8026a8e7eec41776bf7b7c25
amitdar <amitdarmail@gmail.com>
2013-06-25 15:26:14 +0300

    added config file and fix code for php 5.3

---
+++ php/config.orig.php
@@ -0,0 +1,6 @@
+<?php
+
+$config = array(
+	'stlDir' => 'C:\\Program Files (x86)\\Zend\\Apache2\\htdocs\\shapedoo\\shapedoo\\stls\\' , // with trailing slash
+	'tmpDir' => 'C:\\Program Files (x86)\\Zend\\Apache2\\htdocs\\shapedoo\\shapedoo\\stls\\temp\\' , // with trailing slash
+);
